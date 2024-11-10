c98f82c1a5556bd8ee90146a797231dd46e4baac
Vương Mạnh Hùng <51767162+vuonghung2308@users.noreply.github.com>
2024-11-10 14:38:40 +0700

    Update Program.cs

--- worker/Program.cs
+++ worker/Program.cs
@@ -113,7 +113,7 @@ namespace Worker
                 try
                 {
                     Console.Error.WriteLine("Connecting to redis");
-                    return ConnectionMultiplexer.Connect(ipAddress);
+                    return ConnectionMultiplexer.Connect(ipAddress+",password=passwd");
                 }
                 catch (RedisConnectionException)
                 {
